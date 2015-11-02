937bc9e67970dcac7c33ba0fe9347069d0b21cf8
Rhys Bartholomew <rhysb89@gmail.com>
2015-11-02 16:15:53 +1030

    ScriptTesting

---
+++ Programming/ScriptTest/Assets/Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float secondJumpSpeed = 4.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+	public bool canDoubleJump;
+	public int jumpNumb;
+
+	void Start (){
+
+		canDoubleJump = true;
+		jumpNumb = 0;
+	}
+
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded == true) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+
+			canDoubleJump = true;
+			jumpNumb = 0;
+
+		}
+		if (Input.GetButtonDown("Jump") && jumpNumb == 0 && canDoubleJump == true){
+			moveDirection.y = jumpSpeed;
+			jumpNumb += 1;
+
+		}
+		else if (Input.GetButtonDown("Jump") && jumpNumb == 1){
+			moveDirection.y = secondJumpSpeed;
+			jumpNumb += 1;
+			
+		}
+		else if (jumpNumb >= 2) {
+
+				
+				canDoubleJump = false;
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
---
+++ Programming/ScriptTest/Assets/movingPlatform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingPlatform : MonoBehaviour {
+	public float speed = 1f;
+	public Transform platform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		platform.position += new Vector3 (0, Time.deltaTime * speed, 0);
+	}
+	public void OnTriggerEnter (Collider platform)
+	{
+
+		if (platform.tag == "PlatformTop") {
+			speed = speed * -1;
+			Debug.Log ("speedchange");
+		}
+		if (platform.tag == "PlatformBottom") {
+			speed = speed * -1;
+		}
+	}
+}
